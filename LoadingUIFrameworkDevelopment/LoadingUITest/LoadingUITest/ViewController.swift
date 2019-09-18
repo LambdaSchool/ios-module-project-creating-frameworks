03db6f2221450bf325a0ffc791acc241395ca9ac
Jeffrey <SantanaJny@gmail.com>
2019-09-18 17:38:19 -0400

    Creating test project and adding framework

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Jeffrey Santana on 9/18/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
