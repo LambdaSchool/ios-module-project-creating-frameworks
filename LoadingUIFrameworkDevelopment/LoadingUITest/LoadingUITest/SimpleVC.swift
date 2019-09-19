4ee6d17d488fcecf70b3f564e8c7cf893dcc2d2f
Jeffrey <SantanaJny@gmail.com>
2019-09-18 20:53:03 -0400

    Implementing framework

--- LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/SimpleVC.swift
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/SimpleVC.swift
@@ -7,24 +7,46 @@
 //
 
 import UIKit
+import LoadingUI
 
 class SimpleVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	//MARK: - IBOutlets
+	
+	@IBOutlet weak var spinnerView: UIView!
+	@IBOutlet weak var spinner2View: UIView!
+	@IBOutlet weak var spinner3View: UIView!
+	
+	//MARK: - Properties
+	
+	var loaders = [LoadingVC]()
+	
+	//MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setupSpinners(on: [spinnerView, spinner2View, spinner3View])
+	}
+	
+	//MARK: - IBActions
+	
+	@IBAction func spinnerSwitch(_ sender: UISwitch) {
+		if sender.isOn {
+			loaders.forEach({$0.startloading()})
+		} else {
+			loaders.forEach({$0.stopLoading()})
+		}
+	}
+	
+	//MARK: - Helpers
+	
+	private func setupSpinners(on views: [UIView]) {
+		views.forEach{
+			let loader = LoadingVC()
+			loader.setupLoadingView(on: $0)
+			loader.startloading()
+			loaders.append(loader)
+		}
+	}
 }
---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingVC.swift
+//  LoadingUITest
+//
+//  Created by Jeffrey Santana on 9/18/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class LoadingVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
