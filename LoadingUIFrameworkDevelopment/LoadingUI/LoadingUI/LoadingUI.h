ac089711cc110e1a7a9e504a800e3dabf119aa03
ladybeitel <ciarabeitel@gmail.com>
2019-10-16 16:39:44 -0400

    Create LoadingUI Framework and LoadingUITest app. Part 1.

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Ciara Beitel on 10/16/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Ciara Beitel on 10/16/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
