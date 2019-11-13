f14a7d5e4c2e548fbab71ab0b85cc0eed17ee035
Isvvc <lyons@tuta.io>
2019-11-13 14:11:43 -0700

    Create Xcode projects for framework and app

---
+++ Loading/Loading/Loading.h
@@ -0,0 +1,19 @@
+//
+//  Loading.h
+//  Loading
+//
+//  Created by Isaac Lyons on 11/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Loading.
+FOUNDATION_EXPORT double LoadingVersionNumber;
+
+//! Project version string for Loading.
+FOUNDATION_EXPORT const unsigned char LoadingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loading/PublicHeader.h>
+
+
---
+++ LoadingApp/LoadingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingApp
+//
+//  Created by Isaac Lyons on 11/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
