cb8218d950c87e58058e77aff32fd9bf9643b1f2
Gi Pyo Kim <gipyok@gmail.com>
2019-11-13 12:46:09 -0800

    Added LoadingUI to LoadingUIText project

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Gi Pyo Kim on 11/13/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
