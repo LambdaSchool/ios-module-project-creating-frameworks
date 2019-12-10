ecaf3453fb7e96a3e61c87ed04f902b2580b063a
Robert Keffury <bobby.keffury@me.com>
2019-12-10 08:44:39 -0800

    Created LoadingUITest project

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Bobby Keffury on 12/10/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
