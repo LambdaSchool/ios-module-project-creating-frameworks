32d2c5657d8a80ec6473d29f652a4d92e392f1d7
Robert Keffury <bobby.keffury@me.com>
2019-12-10 08:45:36 -0800

    Created LoadingUI Framework

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Bobby Keffury on 12/10/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
