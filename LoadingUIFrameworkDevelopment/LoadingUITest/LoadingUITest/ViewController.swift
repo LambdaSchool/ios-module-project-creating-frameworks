f116d03db3a7c5c58aaf7e6cf8acb5fafeeaaf08
dillonp23 <dillonp.lambda@gmail.com>
2019-12-10 19:05:28 -0800

    Add new test app and framework. Remove the incorrect framework from pervious commit

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Dillon P on 12/10/19.
+//  Copyright © 2019 Lambda iOSPT2. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+
+}
+
