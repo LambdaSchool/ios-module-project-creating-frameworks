77a0900c745ab5f60d6d6a6d5bc8a7bc47657606
dillonp23 <dillonp.lambda@gmail.com>
2019-12-10 21:45:27 -0800

    Add buttons to view and allow to control whether to stop or start loading animation on screen

--- LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 
 public class LoadingViewController: UIViewController {
+    
+    let centerX = UIScreen.main.bounds.size.width*0.5
+    let centerY = UIScreen.main.bounds.size.height*0.5
+    let width: CGFloat = 100.0
+    let height: CGFloat = 100.0
 
     override public func viewDidLoad() {
         super.viewDidLoad()
+        setUpButtons()
     }
     
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         view.backgroundColor = .white
-        let centerX = UIScreen.main.bounds.size.width*0.5
-        let centerY = UIScreen.main.bounds.size.height*0.5
-        let width: CGFloat = 100.0
-        let height: CGFloat = 100.0
         indeterminateLoadingView = IndeterminateLoadingView(frame: CGRect(x: centerX - width/2, y: centerY - height/2, width: width, height: height))
         setUpUI()
         startAnimating()
@@ -28,16 +30,43 @@ public class LoadingViewController: UIViewController {
     
     var indeterminateLoadingView: IndeterminateLoadingView!
    
-    public func startAnimating() {
+    private func startAnimating() {
         indeterminateLoadingView.startAnimating()
     }
     
-    public func stopAnimating() {
+    private func stopAnimating() {
         indeterminateLoadingView.stopAnimating()
     }
     
-    func setUpUI() {
+    private func setUpUI() {
         view.addSubview(indeterminateLoadingView)
+    }
+    
+    private func setUpButtons() {
+        
+        let startButton = UIButton(frame: CGRect(x: centerX-width, y: centerY+width, width: width*2, height: height/2.0))
+        startButton.setTitle("Start Animating", for: .normal)
+        startButton.setTitleColor(.green, for: .normal)
+        startButton.addTarget(self, action: #selector(self.startButton), for: .touchUpInside)
+        
+        let stopButton = UIButton(frame: CGRect(x: centerX-width, y: centerY+width*2, width: width*2, height: height/2.0))
+        stopButton.setTitle("Stop Animating", for: .normal)
+        stopButton.setTitleColor(.red, for: .normal)
+        stopButton.addTarget(self, action: #selector(self.stopButton), for: .touchUpInside)
+        
+        guard let view = view else { return }
+        
+        view.addSubview(startButton)
+        view.addSubview(stopButton)
+        
+    }
+    
+    @objc func startButton() {
+        startAnimating()
+    }
+    
+    @objc func stopButton() {
+        stopAnimating()
     }
 
 }
