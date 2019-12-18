98a6f43e214c6c43371f414c314ad311e460e3a2
Jon Bash <jonbash@protonmail.com>
2019-12-18 14:03:31 -0800

    set up LoadingVC

--- LoadingUI/LoadingUI/LoadingViewController.swift
+++ LoadingUI/LoadingUI/LoadingViewController.swift
@@ -9,9 +9,66 @@
 import UIKit
 
 public class LoadingViewController: UIViewController {
-    public var loadingView: LoadingView = LoadingView()
+    // MARK: - Properties
     
-    override public func viewDidLoad() {
-        super.viewDidLoad()
+    public private(set) var isLoading: Bool = false
+    
+    public weak var delegate: LoadingViewControllerDelegate?
+    public var timeout: TimeInterval = 10
+    
+    private lazy var loadingView: IndeterminateLoadingView? = {
+        let center = CGPoint(
+            x: view.frame.width / 2.0,
+            y: view.frame.height / 2.0)
+        let width = min(center.x, center.y)
+        
+        let loadingViewFrame = CGRect(
+            x: center.x - (width / 2.0),
+            y: center.y - (width / 2.0),
+            width: width,
+            height: width)
+        
+        return IndeterminateLoadingView(frame: loadingViewFrame)
+    }()
+    
+    private weak var timer: Timer?
+    
+    // MARK: - View Lifecycle
+    
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        beginLoading()
+    }
+    
+    public override func viewDidDisappear(_ animated: Bool) {
+        endLoading()
+        
+        super.viewDidDisappear(animated)
+    }
+    
+    // MARK: - Completion
+    
+    public func loadingDidFinish(withError error: Error?) {
+        endLoading()
+        delegate?.loadingViewController(self, didFinishLoadingWithError: error)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func beginLoading() {
+        self.isLoading = true
+        loadingView?.startAnimating()
+        timer = Timer.scheduledTimer(withTimeInterval: timeout, repeats: false, block: { _ in
+            self.delegate?.loadingViewControllerDidTimeOut(self)
+            self.timer = nil
+        })
+        
+    }
+    
+    private func endLoading() {
+        self.isLoading = false
+        loadingView?.stopAnimating()
+        timer?.invalidate()
     }
 }
