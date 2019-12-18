3bd06a2f04945b50a61f82f151d16698f1b830e2
Jon Bash <jonbash@protonmail.com>
2019-12-18 15:56:23 -0800

    attempt further work on LoadingView

--- LoadingUI/LoadingUI/LoadingView.swift
+++ LoadingUI/LoadingUI/LoadingView.swift
@@ -8,18 +8,83 @@
 
 import UIKit
 
+fileprivate let defaultLineWidth: CGFloat = 10
+fileprivate let defaultColor = UIColor.red.cgColor
+
 class LoadingView: UIView {
-    var loadingShape: CAShapeLayer = {
-        var shape = CAShapeLayer()
-        return shape
+    var lineWidth: CGFloat
+    var color: CGColor
+    
+    private(set) var isAnimating: Bool = false
+    
+    lazy var loadingShape: CAShapeLayer = {
+        var shapeLayer = CAShapeLayer()
+        
+        let circlePath = UIBezierPath(
+            arcCenter: self.center,
+            radius: self.frame.width,
+            startAngle: 0,
+            endAngle: 2 * CGFloat.pi,
+            clockwise: true)
+        shapeLayer.path = circlePath.cgPath
+        
+        shapeLayer.strokeEnd = 0
+        
+        shapeLayer.strokeColor = color
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = UIColor.black.cgColor
+        
+        shapeLayer.lineCap = .round
+        
+        layer.addSublayer(shapeLayer)
+        
+        return shapeLayer
     }()
     
-    var progressIsDeterminate = false
-
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        
+    lazy private var animationBlock: (() -> ()) = {
+        UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.6) {
+            self.loadingShape.strokeEnd = 2 * .pi
+        }
+        UIView.addKeyframe(withRelativeStartTime: 0.4, relativeDuration: 0.6) {
+            self.loadingShape.strokeStart = 2 * .pi
+        }
     }
 
+    init(
+        frame: CGRect,
+        lineWidth: CGFloat = defaultLineWidth,
+        color: CGColor = defaultColor)
+    {
+        self.lineWidth = lineWidth
+        self.color = color
+        
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.lineWidth = defaultLineWidth
+        self.color = defaultColor
+        
+        super.init(coder: coder)
+    }
+    
+    func startAnimation() {
+        isAnimating = true
+        animate()
+    }
+    
+    func endAnimation() {
+        isAnimating = false
+    }
+    
+    private func animate() {
+        UIView.animateKeyframes(
+            withDuration: 2,
+            delay: 0,
+            options: [],
+            animations: animationBlock
+        ) { _ in
+            if self.isAnimating { self.animate() }
+        }
+    }
 }
