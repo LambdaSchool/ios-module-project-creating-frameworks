5858e8a39ce7cb4bd323bb54c045c326bba9166c
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-12-18 12:13:58 -0800

    Created LoadingUI Framework and Test App. imported LoadingUI framework into test app view controller and builds successfully

---
+++ LoadingUIFrameworkDevelopment/LoadingUIFramework/LoadingUIFramework/LoadingUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUIFramework.h
+//  LoadingUIFramework
+//
+//  Created by Lambda_School_Loaner_204 on 12/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUIFramework.
+FOUNDATION_EXPORT double LoadingUIFrameworkVersionNumber;
+
+//! Project version string for LoadingUIFramework.
+FOUNDATION_EXPORT const unsigned char LoadingUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUIFramework/PublicHeader.h>
+
+
---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Lambda_School_Loaner_204 on 12/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import LoadingUIFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
