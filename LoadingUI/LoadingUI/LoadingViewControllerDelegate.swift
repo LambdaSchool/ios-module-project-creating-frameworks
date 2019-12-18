4e532ddb67640c33121c712a724620c86d07650f
Jon Bash <jonbash@protonmail.com>
2019-12-18 14:03:05 -0800

    add LoadingVCDelegate

---
+++ LoadingUI/LoadingUI/LoadingViewControllerDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingViewControllerDelegate.swift
+//  LoadingUI
+//
+//  Created by Jon Bash on 2019-12-18.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+public protocol LoadingViewControllerDelegate: class {
+    func loadingViewController(
+        _ loadingViewController: LoadingViewController,
+        didFinishLoadingWithError error: Error?)
+    
+    func loadingViewControllerDidTimeOut(
+        _ loadingViewController: LoadingViewController)
+}
