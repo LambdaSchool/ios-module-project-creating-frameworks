99b95e90e345c0a0e3481cc6226f56a18dc26b9d
Jon Bash <jonbash@protonmail.com>
2019-12-18 08:38:11 -0800

    init example Xcode project

---
+++ LoadingUIExample/LoadingUIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUIExample
+//
+//  Created by Jon Bash on 2019-12-18.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
