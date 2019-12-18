4dbb8e10d62ae1f9cac0213b64fc5adeebc93154
Jerry Haaser <jerry.haaser@hotmail.com>
2019-12-18 17:26:44 -0600

    created framework

---
+++ LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUIFrameworkDevelopment.h
+//  LoadingUIFrameworkDevelopment
+//
+//  Created by Jerry haaser on 12/18/19.
+//  Copyright © 2019 Jerry haaser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUIFrameworkDevelopment.
+FOUNDATION_EXPORT double LoadingUIFrameworkDevelopmentVersionNumber;
+
+//! Project version string for LoadingUIFrameworkDevelopment.
+FOUNDATION_EXPORT const unsigned char LoadingUIFrameworkDevelopmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUIFrameworkDevelopment/PublicHeader.h>
+
+
