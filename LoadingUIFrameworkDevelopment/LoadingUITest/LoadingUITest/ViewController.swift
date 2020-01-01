4d56b50c7fdef707ddc32b10a0472ada3d5273fe
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-01 17:49:57 -0500

    create a framework and a test app

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Vici Shaweddy on 1/1/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
