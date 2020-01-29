704228c912fa83283a69a1780dfd729e172b36ba
Chad Rutherford <chad_rutherford@outlook.com>
2020-01-29 16:06:17 -0500

    Added LoadingUITest Project and LoadingUI Framework

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Chad Rutherford on 1/29/20.
+//  Copyright © 2020 chadarutherford.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
