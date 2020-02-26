a54a9f60845f000bc31b4987a30f84f9ff4cc13a
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-26 14:07:01 -0800

    added framework test app folder

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Jorge Alvarez on 2/26/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
