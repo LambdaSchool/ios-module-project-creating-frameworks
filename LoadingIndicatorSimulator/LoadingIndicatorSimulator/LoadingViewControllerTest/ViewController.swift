8a966e5d2d9081abaeff22229e212a3d1a0fd278
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-26 23:00:58 -0800

    initial commit

---
+++ LoadingIndicatorSimulator/LoadingIndicatorSimulator/LoadingViewControllerTest/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  LoadingIndicatorSimulator
+//
+//  Created by Kenny on 2/26/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+import LoadingIndicator
+
+class ViewController: LoadingViewController {
+    //=======================
+    // MARK: - IBOutlets
+    @IBOutlet weak var redView: UIView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let testRect = CGRect(x: 50,
+                              y: 50,
+                              width: 50,
+                              height: 50)
+        let testView = UIView(frame: testRect)
+        testView.backgroundColor = .systemRed
+        view.addSubview(testView)
+        print(testView.center)
+        load(on: testView)
+        
+        let testRect2 = CGRect(x: 300,
+                              y: 50,
+                              width: 50,
+                              height: 50)
+        let testView2 = UIView(frame: testRect2)
+        testView2.backgroundColor = .systemRed
+        view.addSubview(testView2)
+        load(on: testView2)
+    }
+}
