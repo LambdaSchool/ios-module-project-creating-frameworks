c9712caec271db20307b304461358b205e860a51
Chris <chris.gonzales.82@gmail.com>
2020-03-25 13:45:27 -0700

    Created project with framework

---
+++ LoadingUIFrameworkDevelopment/LoadingUITests/LoadingUITests/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITests
+//
+//  Created by Chris Gonzales on 3/25/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
