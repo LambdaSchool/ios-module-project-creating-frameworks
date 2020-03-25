7466d816ebc8c54bbb65247ef7062c984a49943d
elizabeth paige <wingateep@Icloud.com>
2020-03-25 15:43:18 -0400

    Set up LoadingViewController

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Elizabeth Wingate on 3/25/20.
+//
+
+import UIKit
+
+public class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
