0ee29c639f9ba6508072decdbcbc8fa984d82730
elizabeth paige <wingateep@Icloud.com>
2020-03-25 16:05:03 -0400

    set up LoadingViewController

--- LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import LoadingUI
 
 public class LoadingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+         private var loadingView: IndeterminateLoadingView!
+         
+         public override func viewWillAppear(_ animated: Bool) {
+             super.viewWillAppear(animated)
+             
+             let frame = CGRect(x: 155, y: 325, width: 100, height: 100)
+             
+             loadingView = IndeterminateLoadingView(frame: frame)
+             
+             view.backgroundColor = .white
+             
+             startAnimating()
+             setupUI()
+         }
+         
+         private func startAnimating() {
+             loadingView.startAnimating()
+         }
+         
+         private func setupUI() {
+             view.addSubview(loadingView)
+             
+             let stopButton = UIButton()
+             view.addSubview(stopButton)
+             stopButton.translatesAutoresizingMaskIntoConstraints = false
+             
+             stopButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+             stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+             
+             stopButton.setTitle("Go Back", for: .normal)
+             stopButton.setTitleColor(.red, for: .normal)
+             stopButton.addTarget(self, action: #selector(stopAnimatingAndDismiss), for: .touchUpInside)
+         }
+         
+         @objc private func stopAnimatingAndDismiss() {
+             loadingView.stopAnimating()
+             dismiss(animated: true, completion: nil)
+         }
+         
+     }
