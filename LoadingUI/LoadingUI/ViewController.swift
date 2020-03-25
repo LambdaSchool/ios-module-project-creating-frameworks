653e00087514fb6e24acf7fe3024601aec3c8163
Enrique Gongora <EGTech24@gmail.com>
2020-03-25 13:41:32 -0700

    Added framework

---
+++ LoadingUI/LoadingUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUI
+//
+//  Created by Enrique Gongora on 3/25/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoadingUI/LoadingUIFramework/LoadingUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUIFramework.h
+//  LoadingUIFramework
+//
+//  Created by Enrique Gongora on 3/25/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUIFramework.
+FOUNDATION_EXPORT double LoadingUIFrameworkVersionNumber;
+
+//! Project version string for LoadingUIFramework.
+FOUNDATION_EXPORT const unsigned char LoadingUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUIFramework/PublicHeader.h>
+
+
