19359bffb3011ff240be67d948df53387b4ccaca
Chris <chris.gonzales.82@gmail.com>
2020-03-25 12:40:38 -0700

    Initial commit with framework file

---
+++ LoadingUIFramework/LoadingUIFramework/LoadingUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUIFramework.h
+//  LoadingUIFramework
+//
+//  Created by Chris Gonzales on 3/25/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUIFramework.
+FOUNDATION_EXPORT double LoadingUIFrameworkVersionNumber;
+
+//! Project version string for LoadingUIFramework.
+FOUNDATION_EXPORT const unsigned char LoadingUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUIFramework/PublicHeader.h>
+
+
