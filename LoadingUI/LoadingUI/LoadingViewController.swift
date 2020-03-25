e082a415f385935baf4bc06f149c5389f1827ffa
Nick <ptnguyen1901@gmail.com>
2020-03-25 16:45:18 -0400

    Adding Frameworks, set up UI

---
+++ LoadingUI/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Nick Nguyen on 3/25/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+public class LoadingViewController : UIViewController
+{
+
+  
+    
+}
+
--- LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment/ViewController.swift
+++ LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import LoadingUI
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    private let activateButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .darkText
+        button.setTitle("Activate", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        button.layer.cornerRadius = 16
+        button.addTarget(self, action: #selector(activateTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpUI()
+        
+        
     }
 
-
+    @objc private func activateTapped() {
+        print("Showing modal view")
+    }
+    
+    
+    private func setUpUI() {
+        view.addSubview(activateButton)
+        
+        NSLayoutConstraint.activate([
+            activateButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activateButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activateButton.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 80),
+            activateButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -80)
+            
+            
+        ])
+    }
+    
+    
+    
 }
 
