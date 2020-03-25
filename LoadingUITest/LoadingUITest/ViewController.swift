c46554f0036a08d89201df8039dde16aa90be949
scottharris86 <scottharrisdev@gmail.com>
2020-03-25 18:52:00 -0400

    Part 1 - Create a Framework and Setup a Test App

---
+++ LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by scott harris on 3/25/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
