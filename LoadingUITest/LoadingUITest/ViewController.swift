84e19c5941f097ab5fd57b4854c6f82f4523f30f
scottharris86 <scottharrisdev@gmail.com>
2020-03-25 20:04:40 -0400

    completed

--- LoadingUITest/LoadingUITest/ViewController.swift
+++ LoadingUITest/LoadingUITest/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import LoadingUI
 
 class ViewController: UIViewController {
-
+    
+    let loadingViewController = LoadingViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupButton()
+    }
+    
+    func setupButton() {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(animateTapped), for: .touchUpInside)
+        button.setTitle("Animate", for: .normal)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+  
+    }
+    
+    @objc func animateTapped() {
+        loadingViewController.showLoading()
+        let gestureRec = UITapGestureRecognizer(target: self, action: #selector(stopLoading))
+        loadingViewController.view.addGestureRecognizer(gestureRec)
+        present(loadingViewController, animated: false)
+    }
+    
+    @objc func stopLoading() {
+        loadingViewController.dismissLoading()
     }
 
 
