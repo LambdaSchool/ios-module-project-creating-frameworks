5c40ba48fbceb713d81ac410edef798ede14a16d
Shawn Gee <shawngee@gmail.com>
2020-04-22 17:33:40 -0400

    Added call to present LoadingViewController in viewDidAppear to test functionality

--- LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 import LoadingUI
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        present(LoadingViewController(), animated: true)
+    }
+    
+    
 }
 
