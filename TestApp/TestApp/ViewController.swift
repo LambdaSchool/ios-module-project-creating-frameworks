9cdb2b3fd3cf5df8583eb7643a8f46222a3eb1a2
Brad <braddiroff@icloud.com>
2020-04-22 16:29:49 -0400

    made the two projects

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Bradley Diroff on 4/22/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
