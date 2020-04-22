17ec1f7dd6306be044f25724c4740c1e85d13cbc
Lydia Zhang <art4everyan@gmail.com>
2020-04-22 17:02:00 -0400

    Set up projects

---
+++ LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Lydia Zhang on 4/22/20.
+//  Copyright © 2020 Lydia Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
---
+++ LoadingUIFrameworkDevelopment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUIFrameworkDevelopment
+//
+//  Created by Lydia Zhang on 4/22/20.
+//  Copyright © 2020 Lydia Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
