5b1d71de2b06fd45d1cfd4e2346914204f89a7f9
Hector L <hectoremil21@hotmail.com>
2020-04-22 08:36:04 -0400

    Added provided IndeterminateLoadingView.swift

---
+++ LoadingUIFramework/LoadingUI/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Karen Rodriguez on 4/22/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class LoadingViewController: UIViewController {
+    
+}
