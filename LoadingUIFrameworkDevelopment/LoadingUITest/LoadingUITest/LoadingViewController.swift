6dff35e8fbf38f78778dfbf2333f0db9d99d1fd0
Lydia Zhang <art4everyan@gmail.com>
2020-04-22 18:05:35 -0400

    Made the view

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/LoadingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoadingViewController.swift
+//  LoadingUITest
+//
+//  Created by Lydia Zhang on 4/22/20.
+//  Copyright © 2020 Lydia Zhang. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class LoadingViewController: UIViewController {
+
+    @IBOutlet weak var loadButton: UIButton!
+    
+    var indeterminateLoadingView: IndeterminateLoadingView = {
+        let x = UIScreen.main.bounds.width / 2
+        let y = UIScreen.main.bounds.height / 2
+        let frame: CGRect = CGRect(origin: CGPoint(x: x, y: y), size: CGSize(width: 100, height: 100))
+        return IndeterminateLoadingView(frame: frame)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func startLoad(_ sender: Any) {
+    }
+
+
+}
