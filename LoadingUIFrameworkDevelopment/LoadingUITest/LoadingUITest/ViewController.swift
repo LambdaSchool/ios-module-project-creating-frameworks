4d1ea6131a49dbbb8079c46b18006fd5765e0ea3
Shawn Gee <shawngee@gmail.com>
2020-04-22 16:52:22 -0400

    Added LoadingUI Framework and test project

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Shawn Gee on 4/22/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
