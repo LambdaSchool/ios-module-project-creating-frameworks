cf976aecccc55e984467baa3ba9bf4869f907cdd
Mark Gerrior <github@gerrior.com>
2020-04-22 14:21:49 -0700

    Added projects LoadingUITest and LoadingUI

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Mark Gerrior on 4/22/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
