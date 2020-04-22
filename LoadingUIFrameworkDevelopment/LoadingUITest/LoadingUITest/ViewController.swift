1d1abe9e3718720e692f0326d37a3a8a00dce55a
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-22 16:07:16 -0400

    Project created with embedded framework

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Wyatt Harrell on 4/22/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
