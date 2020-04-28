61f35a4e8131fd735ae9809dd6b8c2ba50e49d7d
David Wright <davidtwright89@gmail.com>
2020-04-28 00:16:56 -0400

    Created LoadingUITest project and added a new framework called LoadingUI

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by David Wright on 4/27/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
