4e8269a0b8f4acd7fb64a95e6741f1995e87437f
Sal <salmanamer@gmail.com>
2020-04-28 18:51:00 -0700

    setup project and framework

---
+++ LoadingScreen/LoadingScreen/LoadingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingViewController.swift
+//  LoadingScreen
+//
+//  Created by Sal B Amer on 4/28/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoadingScreen/LoadingUITests/LoadingUITests.swift
@@ -0,0 +1,34 @@
+//
+//  LoadingUITests.swift
+//  LoadingUITests
+//
+//  Created by Sal B Amer on 4/28/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+import XCTest
+@testable import LoadingUI
+
+class LoadingUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
