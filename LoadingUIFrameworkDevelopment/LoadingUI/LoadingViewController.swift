3ad3d9207aafdf5068ab8f9d53f8bbe3e5487cca
Jessie <jessie.griffin@icloud.com>
2020-04-28 21:38:06 -0400

    Adds public class LoadingViewController

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Jessie Ann Griffin on 4/28/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
