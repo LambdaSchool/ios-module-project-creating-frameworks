aa7d1bca5020542004be3b782e3741ab3490d444
Denis Cedeno <denis.cedeno@gmail.com>
2020-04-30 17:59:51 -0400

    initial project commit

---
+++ LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUIFrameworkDevelopment
+//
+//  Created by denis cedeno on 4/30/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
