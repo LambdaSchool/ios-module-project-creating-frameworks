7667cac0b4464884c5d7dfaa8adcd26d1d7a90f2
Denis Cedeno <denis.cedeno@gmail.com>
2020-05-03 11:50:11 -0400

    created framework

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by denis cedeno on 5/3/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
