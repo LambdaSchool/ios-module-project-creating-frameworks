419aed4f16d1b8fe86c13429cbe20f792346b3be
Denis Cedeno <denis.cedeno@gmail.com>
2020-05-03 12:21:19 -0400

    created loadTest app and embedded LoadUI framework

---
+++ LoadingTest/LoadingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingTest
+//
+//  Created by denis cedeno on 5/3/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
