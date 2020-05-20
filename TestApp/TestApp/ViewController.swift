9d40ff4ac43e983b27ad9221264bf16c1d1eda15
Shawn James <shawn.james@me.com>
2020-05-20 18:34:01 -0400

    Add TestApp project

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Shawn James on 5/20/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
