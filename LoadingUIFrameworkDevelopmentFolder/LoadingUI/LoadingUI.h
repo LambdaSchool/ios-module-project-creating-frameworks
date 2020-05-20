129dde800408ce36be3df57b98f48f3b3beba5c9
Mark Poggi <markp@mac.com>
2020-05-20 12:53:07 -0700

    Dragged framework into project's framework group

---
+++ LoadingUIFrameworkDevelopmentFolder/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Mark Poggi on 5/20/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
