ace99d6514a40decd06a8cc228f6ddf088499fca
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-20 16:12:55 -0400

    Added testing code for basic implementation

--- LoadingUISampleCode/LoadingUISampleCode/ViewController.swift
+++ LoadingUISampleCode/LoadingUISampleCode/ViewController.swift
@@ -15,7 +15,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func presentLoadingScreen(_ sender: UIButton) {
+        let bundle = Bundle(for: LoadingViewController.self)
+        let VC = LoadingViewController(nibName: nil, bundle: bundle)
+        VC.modalPresentationStyle = .fullScreen
+        present(VC, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            VC.dismiss(animated: true, completion: nil)
+        }
+    }
 }
-
