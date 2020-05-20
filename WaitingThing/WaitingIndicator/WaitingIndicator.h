c5b97e6ee058c87a70f6c1561da98ceba94a49c7
Brad <braddiroff@icloud.com>
2020-05-20 16:27:04 -0400

    created the projects

---
+++ WaitingThing/WaitingIndicator/WaitingIndicator.h
@@ -0,0 +1,19 @@
+//
+//  WaitingIndicator.h
+//  WaitingIndicator
+//
+//  Created by Bradley Diroff on 5/20/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaitingIndicator.
+FOUNDATION_EXPORT double WaitingIndicatorVersionNumber;
+
+//! Project version string for WaitingIndicator.
+FOUNDATION_EXPORT const unsigned char WaitingIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaitingIndicator/PublicHeader.h>
+
+
---
+++ WaitingThing/WaitingProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaitingProject
+//
+//  Created by Bradley Diroff on 5/20/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
