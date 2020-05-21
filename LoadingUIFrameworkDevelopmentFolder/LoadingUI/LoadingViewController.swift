aaf393bf51d37f0f277d26a0cba725881e6fc47b
Chris Dobek <dobek.christopher@gmail.com>
2020-05-21 09:39:05 -0400

    Added LoadingVC

---
+++ LoadingUIFrameworkDevelopmentFolder/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Chris Dobek on 5/21/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
