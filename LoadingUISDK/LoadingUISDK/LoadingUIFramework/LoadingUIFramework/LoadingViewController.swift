0ea0aab15cbb86b16693b18eb6915391cf80d464
Matt Martindale <martindalematt18@yahoo.com>
2020-06-09 20:53:01 -0600

    Setup project files and framework

---
+++ LoadingUISDK/LoadingUISDK/LoadingUIFramework/LoadingUIFramework/LoadingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingViewController.swift
+//  LoadingUIFramework
+//
+//  Created by Matthew Martindale on 6/9/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
