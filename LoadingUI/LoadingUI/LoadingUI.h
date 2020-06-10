58841443075cfc766aa76ff7401a8561f5bf38dd
Thomas Dye <thomasallendye@gmail.com>
2020-06-10 19:10:10 -0400

    commits are not working for me :(

---
+++ LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,15 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Thomas Dye on 6/9/20.
+//  Copyright © 2020 Thomas Dye. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
