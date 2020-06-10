25c28534743e82306fbedd832c7a63c94ed8d9ab
Libby Thomas <thomaslibby05@gmail.com>
2020-06-09 20:56:21 -0500

    Initial Commit

---
+++ LoadingUI/LoadingUI/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUI
+//
+//  Created by Elizabeth Thomas on 6/9/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
