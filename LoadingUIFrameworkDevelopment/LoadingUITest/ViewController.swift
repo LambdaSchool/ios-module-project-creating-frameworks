3f5b17417e95cead3704277325ee8c42b8f0519b
Claudia Maciel <clc_80@yahoo.com>
2020-06-09 19:13:50 -0700

    created a framework and setup a test app

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Claudia Contreras on 6/9/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
