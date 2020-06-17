500bca5366266fe55376d9417e26d83983c9d1ff
Kelson Hartle <kelson99hartle@icloud.com>
2020-06-16 23:15:58 -0600

    Added project files including framework project and test xcode project

---
+++ LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUIFrameworkDevelopment.h
+//  LoadingUIFrameworkDevelopment
+//
+//  Created by Kelson Hartle on 6/16/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUIFrameworkDevelopment.
+FOUNDATION_EXPORT double LoadingUIFrameworkDevelopmentVersionNumber;
+
+//! Project version string for LoadingUIFrameworkDevelopment.
+FOUNDATION_EXPORT const unsigned char LoadingUIFrameworkDevelopmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUIFrameworkDevelopment/PublicHeader.h>
+
+
---
+++ loadingUITestFrameworkProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loadingUITestFrameworkProject
+//
+//  Created by Kelson Hartle on 6/16/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
