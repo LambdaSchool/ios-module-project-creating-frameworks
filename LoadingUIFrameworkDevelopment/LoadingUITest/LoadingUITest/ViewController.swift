9cc8e3e35782aa74a6ec0fd1320ae330ff13a182
Harmony Radley <harmonyradley99@gmail.com>
2020-06-18 18:14:39 -0400

    Followed Part 1 of assignment

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Harmony Radley on 6/18/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+import LoadingUI 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
