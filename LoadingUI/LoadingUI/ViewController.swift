0b358e7fc838dc1e997929111feda7539e178a57
James McDougall <jameswiliammcdougall@gmail.com>
2020-08-11 20:41:39 -0500

    Initial Project Commit

---
+++ LoadingUI/LoadingUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUI
+//
+//  Created by Lambda_School_loaner_226 on 8/11/20.
+//  Copyright © 2020 LoadingUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
