f86e9b460aa6c32b1b8c208b093fc22a9425de39
Clayton Watkins <watkinclayton17@yahoo.com>
2020-08-11 19:05:38 -0700

    Created Blank Framework ' LoadingUI' and imported it into project

---
+++ LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LoadingUIFrameworkDevelopment
+//
+//  Created by Clayton Watkins on 8/11/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
