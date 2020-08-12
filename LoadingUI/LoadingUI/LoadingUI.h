152d4b337af793b9c47cdb1c0dd212114793d4bd
Robert Vance <Fearmydotzs@gmail.com>
2020-08-11 22:09:52 -0600

    Completed Project

---
+++ LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Rob Vance on 8/11/20.
+//  Copyright © 2020 Robs Creations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
---
+++ LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Rob Vance on 8/11/20.
+//  Copyright © 2020 Robs Creations. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
+
