0236d28566fa9a7b7c9a700f566226c64d63acc2
BrysonSaclausa <brys1saclaus2@gmail.com>
2020-10-06 13:05:51 -0700

    Part 1: able to 'import LoadingUI'

---
+++ LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by BrysonSaclausa on 10/6/20.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
