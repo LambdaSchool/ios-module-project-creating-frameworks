076392a0187220e84314961c0afa76413d2e12f9
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-10-05 23:02:30 -0700

    Set up View Controllers

---
+++ LoadingUI/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Cora Jacobson on 10/5/20.
+//
+
+import UIKit
+
+public class LoadingViewController: UIViewController {
+    
+    public var duration: CGFloat = 5
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+
+}
--- LoadingUITest/LoadingUITest/ViewController.swift
+++ LoadingUITest/LoadingUITest/ViewController.swift
@@ -10,11 +10,37 @@ import LoadingUI
 
 class ViewController: UIViewController {
 
+    var animationDuration: CGFloat = 5
+    let loadingViewController = LoadingViewController()
+    
+    @IBOutlet weak var animationLengthLabel: UILabel!
+    @IBOutlet weak var animationLengthSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationLengthSlider.value = 5
+        loadingViewController.duration = animationDuration
     }
 
-
+    @IBAction func animationLengthSelected(_ sender: UISlider) {
+        animationDuration = CGFloat(sender.value)
+        loadingViewController.duration = CGFloat(sender.value)
+        let labelValue = Int(sender.value)
+        if labelValue > 1 {
+            animationLengthLabel.text = "Animation Length: \(labelValue) seconds"
+        } else {
+            animationLengthLabel.text = "Animation Length: \(labelValue) second"
+        }
+    }
+    
+    @IBAction func theButtonWasTapped(_ sender: UIButton) {
+        self.present(loadingViewController, animated: true, completion: nil)
+        perform(#selector(dismissModalVC), with: nil, afterDelay: TimeInterval(animationDuration + 1))
+    }
+    
+    @objc func dismissModalVC() {
+        self.dismiss(animated:true, completion: nil)
+    }
+    
 }
 
